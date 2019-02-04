b2dee656e2cf1176d449a4903d42d40a19091d95
Oliver Blake <P17158059@my365.dmu.ac.uk>
2019-02-04 17:27:29 +0000

    Creation of clsAddress plus test class

---
+++ EmployeeTestProject/tstAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectClassLibrary;
+
+namespace EmployeeTestProject
+{
+    [TestClass]
+    public class tstAddress
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsAddress Anaddress = new clsAddress();
+            Assert.IsNotNull(Anaddress);
+        }
+    }
+}
